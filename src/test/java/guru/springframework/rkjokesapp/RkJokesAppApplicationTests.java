b17386faa217dfd711bc930db0d0d8a0b0f6b093
Venkata Ramanuja Rao Kondapally <vkondapa@cisco.com>
2021-10-07 22:01:04 +0530

    RK Jokes Application

---
+++ src/test/java/guru/springframework/rkjokesapp/RkJokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.rkjokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RkJokesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
